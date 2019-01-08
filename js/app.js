5c891e08d52dfe30750b6f411b3d59cc230fd6bd
Alyssa Voccia <avoccia@hotmail.com>
2019-01-08 16:44:21 -0500

    Added animation to the bell icon and setup the notification content

--- js/app.js
+++ js/app.js
@@ -3,4 +3,13 @@ const exitAlert = document.querySelector('.exit-alert');
 const alertBox = document.querySelector('.alert-box');
 exitAlert.addEventListener('click', (e) => {
   alertBox.style.display = 'none'; 
-})
+});
+
+// Event listener for when a user clicks on the bell icon
+const bell = document.querySelector('.bell-icon');
+const bellBadge = document.querySelector('.bell-badge');
+const notificationContent = document.querySelector('.notification-content');
+bell.addEventListener('click', () => {
+  notificationContent.classList.toggle('show');
+  bellBadge.classList.add('hide');
+});
