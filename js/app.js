40375833f388e3cc57873eed3ca9a9644b267080
Alyssa Voccia <avoccia@hotmail.com>
2019-01-08 19:14:58 -0500

    Completed the message user section
    Added javascript validation and added autocomplete functionality for searching a user.

--- js/app.js
+++ js/app.js
@@ -13,3 +13,29 @@ bell.addEventListener('click', () => {
   notificationContent.classList.toggle('show');
   bellBadge.classList.add('hide');
 });
+
+// Validation for Message form
+const sendButton = document.querySelector('.message-button');
+sendButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let userSearch = document.forms['message-user-form']['search-user'].value;
+  let userMessage = document.forms['message-user-form']['message-user'].value;
+  let userAlert = document.querySelector('.user-alert');
+  let messageAlert = document.querySelector('.message-alert');
+  let successMessage = document.querySelector('.success-message');
+  if (userSearch === '') {
+    userAlert.style.display = 'block';
+  } else {
+    userAlert.style.display = 'none';
+  }
+  if (userMessage === '') {
+    messageAlert.style.display = 'block';
+  } else {
+    messageAlert.style.display = 'none';
+  }
+
+  if (userSearch !== '' && userMessage !== '') {
+    successMessage.style.display = 'block';
+    document.getElementById('message-form').reset();
+  }
+});
